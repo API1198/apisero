b8844a10faf1bd9597a3b7785d527e07bb2a1245
API1198 <rajashekar.gundu@apisero.com>
2021-06-02 03:40:52 -0700

    9

--- demo.java
+++ demo.java
@@ -5,12 +5,12 @@ public static void main(String[] args)
 System.out.print("hey");
 System.out.print("hey");
 System.out.print("added from aoisero brannch");
-<<<<<<< HEAD
+
 System.out.print("added from jenkins");
 System.out.print("added from jenkins");System.out.print("added from jenkins");
 }
-=======
+
 System.out.print("added from remote repository");}
->>>>>>> fac0ca1abf631913a120e731a2514dc0ef2b4cff
+
 
 }
